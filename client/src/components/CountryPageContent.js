3d72c8e5b3aab743d25c3748cdddd4553b8cf7af
SimaxSaab <minoreat@gmail.com>
2021-03-13 23:17:28 +0200

    error: currentGallery is undefined

--- client/src/components/CountryPageContent.js
+++ client/src/components/CountryPageContent.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -13,14 +13,27 @@ export default function CountryPageContent() {
   const { name } = useParams();
   const country = countries.find((el) => el.name.toLowerCase() === name);
   const dispatch = useDispatch();
-  dispatch(countryReq(country.name));
+  useEffect(() => {
+    dispatch(countryReq(country.name));
+  }, [dispatch, country.name]);
+  // dispatch(countryReq(country.name));
   const currentInfo = useSelector((state) => state.countriesStore.currentCountry.info);
+  const currentPhoto = useSelector((state) => state.countriesStore.currentCountry.photo);
+  const currentGallery = useSelector((state) => state.countriesStore.currentCountry.gallery);
+  // console.log(typeof currentGallery);
 
   return (
     <div>
       <h2>{country.capital}</h2>
       <h3>{currentInfo}</h3>
       <h4>{country.name}</h4>
+      <img src={currentPhoto} alt="Somebody" />
+      <img src={currentGallery[0]} alt="Somebody" />
+      <img src={currentGallery[1]} alt="Somebody" />
+      <img src={currentGallery[2]} alt="Somebody" />
+      <img src={currentGallery[3]} alt="Somebody" />
+      <img src={currentGallery[4]} alt="Somebody" />
+      <img src={currentGallery[5]} alt="Somebody" />
       <button type="button" className="authorization__btn" onClick={() => dispatch(countryReq(country.name))}>Get country</button>
       <MapComponent country={country} />
     </div>
