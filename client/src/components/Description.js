9e69d013d9f7031810437f6dfe8ebc0a79e4597e
SimaxSaab <minoreat@gmail.com>
2021-03-15 17:20:43 +0200

    feat: add description by photo

--- client/src/components/Description.js
+++ client/src/components/Description.js
@@ -1,12 +1,11 @@
+/* eslint-disable react/no-danger */
 /* eslint-disable max-len */
 import React from 'react';
 
 export default function Description({ info }) {
   return (
     <div className="description-component">
-      <p>
-        {info}
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: info }} />
     </div>
   );
 }
