9d029333bd190d03687a99a7a52e5664fdda7aa9
AStashevskaya <stashevskayaaa@gmail.com>
2021-03-17 21:18:34 +0300

    refactor: refactor videocomponent

--- client/src/components/VideoComponent.js
+++ client/src/components/VideoComponent.js
@@ -1,12 +1,9 @@
-/* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React from 'react';
 import ReactPlayer from 'react-player';
 
 import './scss/Video.scss';
 
 export default function VideoComponent({ country }) {
-  const [control, setControl] = useState(true);
-
   const url = `https://slavajsfe.github.io/screenshots/${country}.mp4`;
 
   return (
@@ -14,7 +11,7 @@ export default function VideoComponent({ country }) {
       url={url}
       config={{ file: { attributes: { controlsList: 'nodownload' } } }}
       onContextMenu={(e) => e.preventDefault()}
-      controls={control}
+      controls
     />
   );
 }
