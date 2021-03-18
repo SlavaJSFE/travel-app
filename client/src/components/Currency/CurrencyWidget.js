4d86ecd9f83b06663da0c860ee3463b9c844f7db
SlavaJSFE <viachaslau.shpileuski@gmail.com>
2021-03-18 19:21:58 +0300

    feat: add styles for side bar

--- client/src/components/Currency/CurrencyWidget.js
+++ client/src/components/Currency/CurrencyWidget.js
@@ -1,15 +1,20 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import './Currency.scss';
+import { useTranslation } from 'react-i18next';
 
 import CurrencyRow from './CurrencyRow';
 
 export default function CurrencyWidget({ currency, code }) {
   const { rates, base } = currency;
   const { USD, RUB, EUR } = rates;
+  const { t } = useTranslation();
+  const translate = t;
 
   return (
     <div className="currency-widget">
+      <div className="currency-title">
+        {translate('Currency Exchange')}
+      </div>
       <CurrencyRow code={code} base={base} rate={1} />
       <CurrencyRow code="EU" base="EUR" rate={base === 'EUR' ? 1 : EUR.toFixed(2)} />
       <CurrencyRow code="US" base="USD" rate={USD.toFixed(2)} />
